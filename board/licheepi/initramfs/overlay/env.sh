4f918d09acf2dc0758199d65468ba40bd55d549f
Yusen Hu <richardyusen@gmail.com>
2022-07-29 16:05:17 +0800

    Create standalone environment setup script for initramfs

---
+++ board/licheepi/initramfs/overlay/env.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+export RED='\033[0;31m'
+export GREEN='\033[0;32m'
+export BLUE='\033[0;34m'
+export NC='\033[0m'
+
+export SSH_SRV='richard@192.168.123.11'
+export SSH_DIR='~/outbu'
+export SSH_PORT='2222'
+
