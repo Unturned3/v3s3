506d61d8824f8fd12aed529da8425c13695f6e2c
Yusen Hu <richardyusen@gmail.com>
2022-07-30 12:09:47 +0800

    Add 'quiet' flag to kernel bootargs

--- board/licheepi/boot.cmd
+++ board/licheepi/boot.cmd
@@ -1,4 +1,4 @@
-setenv bootargs console=ttyS0,115200 panic=5 rootwait rdinit=/init initrd=0x42000000 root=/dev/mmcblk0p2 earlyprintk rw
+setenv bootargs console=ttyS0,115200 panic=5 rootwait rdinit=/init initrd=0x42000000 root=/dev/mmcblk0p2 earlyprintk rw quiet
 load mmc 0:1 0x41000000 zImage
 load mmc 0:1 0x41800000 sun8i-v3s-licheepi-zero-dock.dtb
 load mmc 0:1 0x42000000 initramfs.cpio.uboot
