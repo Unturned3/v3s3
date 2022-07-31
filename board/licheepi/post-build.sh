7fc7aa6d518435bc2111204ba23ea06ec5f816e8
Yusen Hu <richardyusen@gmail.com>
2022-07-31 18:00:56 +0800

    Add if-check to post-build script

--- board/licheepi/post-build.sh
+++ board/licheepi/post-build.sh
@@ -6,6 +6,8 @@ EP=$BR2_EXTERNAL_V3S3_PATH
 echo "Copying initramfs.cpio.uboot from ${EP}/board/licheepi/initramfs to ${IP}"
 cp ${EP}/board/licheepi/initramfs/initramfs.cpio.uboot ${IP}/../images
 
-echo "Renaming '$TARGET_DIR/etc/network/if-pre-up.d/wait_iface' script"
-mv $TARGET_DIR/etc/network/if-pre-up.d/wait_iface $TARGET_DIR/etc/network/if-pre-up.d/001-wait_iface
+if [[ -e $TARGET_DIR/etc/network/if-pre-up.d/wait_iface ]]; then
+	echo "Renaming '$TARGET_DIR/etc/network/if-pre-up.d/wait_iface' script"
+	mv $TARGET_DIR/etc/network/if-pre-up.d/wait_iface $TARGET_DIR/etc/network/if-pre-up.d/001-wait_iface
+fi
 
