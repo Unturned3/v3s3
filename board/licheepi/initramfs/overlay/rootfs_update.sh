5b0eabbc4e61e69d6d9fe7b7cb4b749f4d17402e
Yusen Hu <richardyusen@gmail.com>
2022-07-31 18:00:29 +0800

    Use mmcblk0p3 as temporary storage partition during rootfs updates

--- board/licheepi/initramfs/overlay/rootfs_update.sh
+++ board/licheepi/initramfs/overlay/rootfs_update.sh
@@ -2,25 +2,31 @@
 
 source /env.sh
 
-echo -e "Mounting /dev/mmcblk0p2 at /mnt"
-mount /dev/mmcblk0p2 /mnt
+mkdir -p /mnt/p2
+mkdir -p /mnt/p3
+echo -e "Mounting /dev/mmcblk0p{2,3}"
+mount /dev/mmcblk0p2 /mnt/p2
+mount /dev/mmcblk0p3 /mnt/p3
 
 echo -e "Downloading rootfs.tar from ${SSH_SRV}..."
-scp -P $SSH_PORT ${SSH_SRV}:${SSH_DIR}/rootfs.tar /mnt
+scp -P $SSH_PORT ${SSH_SRV}:${SSH_DIR}/rootfs.tar /mnt/p3
 
 if [ $? == 0 ]; then
 	echo -ne "${BLUE}Download complete. Proceed with update (y/N)? ${NC}"
 	read ans
 	if [ $ans == 'y' ] || [ $ans == 'Y' ]; then
-		tar -C /mnt -xf /mnt/rootfs.tar
+		rm -rf /mnt/p2/*
+		tar -C /mnt/p2 -xf /mnt/p3/rootfs.tar
 		echo -e "${GREEN}Update complete.${NC}"
 	else
 		echo -e "${RED}Update aborted.${NC}"
 	fi
-	rm -f /mnt/rootfs.tar
+	rm -f /mnt/p3/rootfs.tar
 else
 	echo -e "${RED}Download failed!${NC}"
 fi
 
-echo -e "Unmounting /dev/mmcblk0p2"
-umount /mnt
+echo -e "Unmounting /dev/mmcblk0p{2,3}"
+umount /mnt/p2
+umount /mnt/p3
+
