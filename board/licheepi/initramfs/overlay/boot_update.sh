49997c78c11401521ba5c2b8fde9244c1defbedb
Yusen Hu <richardyusen@gmail.com>
2022-08-04 22:51:45 +0800

    Improve OTA boot upgrade features

--- board/licheepi/initramfs/overlay/boot_update.sh
+++ board/licheepi/initramfs/overlay/boot_update.sh
@@ -2,25 +2,38 @@
 
 source /env.sh
 
-echo -e "Mounting /dev/mmcblk0p1 at /mnt"
-mount /dev/mmcblk0p1 /mnt
+BOOT_FILES="\
+boot.scr,\
+sun8i-v3s-licheepi-zero.dtb,\
+sun8i-v3s-licheepi-zero-dock.dtb,\
+zImage,\
+initramfs.cpio.uboot\
+"
 
-echo -e "Downloading boot.tar from ${SSH_SRV}..."
-scp -P $SSH_PORT ${SSH_SRV}:${SSH_DIR}/boot.tar /
+SPL="u-boot-sunxi-with-spl.bin"
+
+mkdir /boot_files
+echo -e "Downloading boot files from ${SSH_SRV}..."
+scp -P $SSH_PORT ${SSH_SRV}:${SSH_DIR}/{${BOOT_FILES},${SPL}} /boot_files
 
 if [ $? == 0 ]; then
 	echo -ne "${BLUE}Download complete. Proceed with update (y/N)? ${NC}"
 	read ans
 	if [ $ans == 'y' ] || [ $ans == 'Y' ]; then
-		tar -C /mnt -xf /boot.tar
-		echo -e "${GREEN}Update complete.${NC}"
+		dd if=/boot_files/$SPL of=/dev/mmcblk0 bs=1024 seek=8
+		sync
+		rm /boot_files/$SPL
+		echo -e "Mounting /dev/mmcblk0p1 at /mnt"
+		mount /dev/mmcblk0p1 /mnt
+		cp -f /boot_files/* /mnt
+		echo -e "${GREEN}Update complete.${NC} Unmounting /dev/mmcblk0p1..."
+		umount /mnt
 	else
 		echo -e "${RED}Update aborted.${NC}"
 	fi
-	rm -f /boot.tar
 else
 	echo -e "${RED}Download failed!${NC}"
 fi
 
-echo -e "Unmounting /dev/mmcblk0p1"
-umount /mnt
+rm -rf /boot_files
+
