29596fa953bab3bf4d53a12a09060d28fc981972
Yusen Hu <richardyusen@gmail.com>
2022-08-04 22:48:47 +0800

    Add proper uboot env loading/saving support on mmc0

--- board/licheepi/uboot/boot.cmd
+++ board/licheepi/uboot/boot.cmd
@@ -1,5 +1,18 @@
-setenv bootargs console=ttyS0,115200 panic=5 rootwait rdinit=/init initrd=0x42000000 root=/dev/mmcblk0p2 earlyprintk rw quiet
+
+setenv bootargs console=ttyS0,115200 panic=5 rootwait root=/dev/mmcblk0p2 earlyprintk rw
+
+if test "$quiet" = "1"; then
+	setenv bootargs "${bootargs} quiet"
+fi
+
 load mmc 0:1 0x41000000 zImage
 load mmc 0:1 0x41800000 sun8i-v3s-licheepi-zero-dock.dtb
-load mmc 0:1 0x42000000 initramfs.cpio.uboot
-bootz 0x41000000 0x42000000 0x41800000
+
+if test "$initramfs" = "1"; then
+	setenv bootargs "${bootargs} rdinit=/init initrd=0x42000000"
+	load mmc 0:1 0x42000000 initramfs.cpio.uboot
+	bootz 0x41000000 0x42000000 0x41800000
+else
+	bootz 0x41000000 - 0x41800000
+fi
+
