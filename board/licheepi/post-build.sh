3ca83695ba75e237db5f0e44048f5bf6456f6296
Yusen Hu <richardyusen@gmail.com>
2022-09-09 17:25:33 +0100

    Fix post-build script bugs

--- board/licheepi/post-build.sh
+++ board/licheepi/post-build.sh
@@ -1,15 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 
-IP=$1	# path to outputs/images directory
+IP=$1	# path to outputs/target directory
 EP=$BR2_EXTERNAL_V3S3_PATH
 
-source $BR2_CONFIG &> /dev/null
+source ${BR2_CONFIG} &> /dev/null
 
 if [[ -v BR2_EXTERNAL_ENABLE_INITRAMFS ]]; then
 	echo "Copying initramfs.cpio.uboot from ${EP}/board/licheepi/initramfs to ${IP}"
-	cp ${EP}/board/licheepi/initramfs/initramfs.cpio.uboot ${IP}/images
+	cp ${EP}/board/licheepi/initramfs/initramfs.cpio.uboot ${IP}/../images
 else
-	touch ${IP}/images/initramfs.cpio.uboot
+	touch ${IP}/../images/initramfs.cpio.uboot
 fi
 
 if [[ -e $TARGET_DIR/etc/network/if-pre-up.d/wait_iface ]]; then
