0a526c74224ca06be31f84433b4b40221e6c44a9
Yusen Hu <richardyusen@gmail.com>
2022-09-09 23:10:09 +0800

    Add initramfs enable menu logic to post-build script

--- board/licheepi/post-build.sh
+++ board/licheepi/post-build.sh
@@ -3,10 +3,14 @@
 IP=$1	# path to outputs/images directory
 EP=$BR2_EXTERNAL_V3S3_PATH
 
-echo "BR2_CONFIG: ${BR2_CONFIG}"
+source $BR2_CONFIG &> /dev/null
 
-echo "Copying initramfs.cpio.uboot from ${EP}/board/licheepi/initramfs to ${IP}"
-cp ${EP}/board/licheepi/initramfs/initramfs.cpio.uboot ${IP}/../images
+if [[ -v BR2_EXTERNAL_ENABLE_INITRAMFS ]]; then
+	echo "Copying initramfs.cpio.uboot from ${EP}/board/licheepi/initramfs to ${IP}"
+	cp ${EP}/board/licheepi/initramfs/initramfs.cpio.uboot ${IP}/images
+else
+	touch ${IP}/images/initramfs.cpio.uboot
+fi
 
 if [[ -e $TARGET_DIR/etc/network/if-pre-up.d/wait_iface ]]; then
 	echo "Renaming '$TARGET_DIR/etc/network/if-pre-up.d/wait_iface' script"
